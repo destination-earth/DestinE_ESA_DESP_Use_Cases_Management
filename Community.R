1420940e2d401097205f08ffc59aeacd1ed88513
sofiacatracchia <131154220+sofiacatracchia@users.noreply.github.com>
2024-04-08 16:29:36 +0200

    Update Community.R
    Updated version of the script following the email of the 8 April 2024

--- Community.R
+++ Community.R
@@ -11,12 +11,12 @@ library(sf)
 library(countrycode)
 
 # Set the working directory to the raw_data folder
-setwd("/cloud/project/data/raw_data")
+setwd("C:/Users/Luigi/Desktop/LUIGI/DestinE/TASKS/COMMUNITY DATABASE/R Script from Claudia/RUN R/20240408/Files")
 
 # Read data from the 1st DestinE User eXchange registration form
 db01 <- readxl::read_excel("20230215_User_eXchange_01.xlsx") %>%
   select(`Date Registered`, `Primary Address - Country`, # mandatory fields
-         `First Name`, `Last Name`, `Organization`, `Primary Email`) %>%
+         `First Name`, `Last Name`, `Organization`, `Primary Email`) %>% # temp fields (to remove duplicates)
   rename(Date = `Date Registered`, Country = `Primary Address - Country`,
          First = `First Name`, Last = `Last Name`,
          Org = `Organization`, Email = `Primary Email`) %>%
@@ -64,8 +64,27 @@ db05 <- read.csv("20231213_Roadshow_Webinar_02.csv") %>%
   mutate(across(everything(), tolower)) %>%
   mutate(Date = as.Date(ymd_hms(Date)))
 
-# Read data from the Community registration form - updated weekly
-db06 <- read.csv("20240307_Community_Form.csv") %>%
+# Read data from the Community Insight webinar
+db06 <- read.csv("20240326_Community_Insights.csv") %>%
+  select(`Date`, `Country`,
+         `your.name`, `your.surname`, `Organisation`, `your.email`) %>%
+  rename(First = `your.name`, Last = `your.surname`,
+         Org = `Organisation`, Email = `your.email`) %>%
+  mutate(across(everything(), tolower)) %>%
+  mutate(Date = as.Date(ymd_hms(Date)))
+
+# Read data from the Community registration form - first version without country
+#db06 <- read.csv("20240107_Community_Form_no_country.csv") %>%
+# select(`Date`, `your.country`,
+#        `first.name`, `Lastname`, `Organisation`, `your.email`) %>%
+# rename(Country = `your.country`,
+#        First = `first.name`, Last = `Lastname`,
+#        Org = `Organisation`, Email = `your.email`) %>%
+#  mutate(across(everything(), tolower)) %>% 
+# mutate(Date = as.Date(ymd_hms(Date)))
+
+# Read data from the Community registration form - updated on 20240206
+db07 <- read.csv("20240404_Community_Form.csv") %>%
   select(`Date`, `your.country`,
          `first.name`, `Lastname`, `Organisation`, `your.email`) %>%
   rename(Country = `your.country`,
@@ -75,11 +94,10 @@ db06 <- read.csv("20240307_Community_Form.csv") %>%
   mutate(Date = as.Date(ymd_hms(Date)))
 
 # Merge databases
-df <- rbind(db01, db02, db03, db04, db05, db06) %>%
-  #remove test rows (emptyname/surname)
-  filter(!(is.na(First) | First == "") & !(is.na(Last) | Last == "")) %>%
-  distinct(Email, .keep_all = TRUE) #%>% # remove duplicate email
-  #distinct(First, Last, .keep_all = TRUE) # remove duplicate first/last names
+df <- rbind(db01, db02, db03, db04, db05, db06, db07) %>%
+  filter(!(is.na(First) | First == "") & !(is.na(Last) | Last == "")) %>% #remove test rows (emptyname/surname)
+  distinct(Email, .keep_all = TRUE) %>% # remove duplicate email
+  distinct(First, Last, .keep_all = TRUE) # remove duplicate first/last names
 
 # Summarize registrations by date and calculate cumulative registrations
 df %>%
@@ -90,8 +108,7 @@ df %>%
   arrange(Date) %>%
   mutate(cumulative_registrations = cumsum(Registrations)) %>%
   ggplot(aes(Date, cumulative_registrations)) +
-  geom_line() + scale_color_discrete(name = "") +
-  ylab("Registered community members")
+  geom_line() + scale_color_discrete(name = "") + ylab("Registered community members")
 
 # Fix manually missing values ##################################################
 # Homogenize missing values
@@ -143,47 +160,35 @@ world <- ne_countries(scale = "medium", returnclass = "sf")
 world$admin <- tolower(world$admin)
 
 # Merge your data with the world map
-merged_data <- merge(world, df_country,
-                     by.x = "admin", by.y = "Country", all.x = TRUE)
+merged_data <- merge(world, df_country, by.x = "admin", by.y = "Country", all.x = TRUE)
 
 ggplot() +
   # Plot filled countries with data
   geom_sf(data = merged_data, aes(fill = n), color = "white", size = 0.1) +
   # Plot outlines for all countries
   geom_sf(data = world, fill = NA, color = "black", size = 0.1) +
-  scale_fill_continuous(name = "Number of Entries",
-                        na.value = "grey90", low = "blue", high = "red") +
+  scale_fill_continuous(name = "Number of Entries", na.value = "grey90", low = "blue", high = "red") +
   labs(title = "DestinE community registrations",
        subtitle = "Number of Entries per Country") +
   theme_minimal() +
   theme(legend.position = "bottom")
 
-################################################################################
-# WORD CLOUD
-# install.packages("wordcloud")
-# install.packages("tm")  # For text mining
-library(wordcloud)
-library(tm)
+if (!file.exists("csv_output")) {
+  dir.create("csv_output")
+}
 
-# Example vector of text
-texts <- read.csv("20240307_Community_Form.csv")$Whichtopicsareyouinterestedin
+df$Date <- as.Date(df$Date)
+df_sorted <- df[order(df$Date), ]
 
-# Create a corpus from the vector
-corpus <- Corpus(VectorSource(texts))
+cumulative_data <- df_sorted %>%
+  select(Date) %>%
+  mutate(Registrations = 1) %>%
+  group_by(Date) %>%
+  summarise(Registrations = sum(Registrations)) %>%
+  arrange(Date) %>%
+  mutate(cumulative_registrations = cumsum(Registrations))
 
-# Preprocess the corpus
-corpus <- tm_map(corpus, content_transformer(tolower))
-corpus <- tm_map(corpus, removePunctuation)
-corpus <- tm_map(corpus, removeWords, stopwords("english"))
 
-# Convert the corpus to a plain text document
-dtm <- TermDocumentMatrix(corpus)
-matrix <- as.matrix(dtm)
-word_freqs <- sort(rowSums(matrix), decreasing = TRUE)
-df_word_freqs <- data.frame(word = names(word_freqs), freq = word_freqs)
-
-# Generate the word cloud
-wordcloud(words = df_word_freqs$word, freq = df_word_freqs$freq, min.freq = 1,
-          max.words=200, random.order=FALSE, rot.per=0.35, 
-          colors=brewer.pal(8, "Dark2"))
-
+write.csv(cumulative_data, file = "csv_output/cumulative_registrations.csv", row.names = FALSE)
+df_country <- df_country %>% arrange(Country)
+write.csv(df_country, file = "csv_output/destine_country_counts.csv", row.names = FALSE)
